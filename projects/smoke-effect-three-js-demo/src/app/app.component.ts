f771b906cfac44e02b505221a354b14f0f908464
Craig Wayne <craigiswayne@gmail.com>
2025-02-27 23:10:41 +0000

    Updated smoke particle effects with gui controls

--- projects/smoke-effect-three-js-demo/src/app/app.component.ts
+++ projects/smoke-effect-three-js-demo/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import {Component, ElementRef, HostBinding, HostListener, inject, OnDestroy, OnInit, viewChild} from '@angular/core';
 import * as THREE from 'three';
-import {Clock, Mesh, MeshLambertMaterial, PerspectiveCamera, Scene, WebGLRenderer, PlaneGeometry} from 'three';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
-import {merge, takeLast, tap} from 'rxjs';
+import {
+  Clock,
+  Mesh,
+  MeshLambertMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  PlaneGeometry,
+  Texture,
+  Vector3, BufferGeometry, Line, Vector2, LineBasicMaterial
+} from 'three';
+import GUI from 'lil-gui';
+import {LineMaterial} from 'three/examples/jsm/lines/LineMaterial.js';
 
 const random_number_in_range = (min: number, max: number): number => {
   return Math.random() * (max - min) + min;
@@ -42,9 +52,6 @@ const presets: {
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [
-    ReactiveFormsModule
-  ],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
@@ -79,7 +86,7 @@ export class AppComponent implements OnInit, OnDestroy {
     // console.log('x%', x_percent);
     // console.log('y%', y_percent);
     //
-    console.log('x/y', rotate_x, rotate_y)
+    // console.log('x/y', rotate_x, rotate_y)
 
     this.sky_image_ref()!.nativeElement.style.setProperty('--sky-rotate-x', `${rotate_x}deg`);
     this.sky_image_ref()!.nativeElement.style.setProperty('--sky-rotate-y', `${rotate_y}deg`);
@@ -91,78 +98,79 @@ export class AppComponent implements OnInit, OnDestroy {
   private scene: Scene = new THREE.Scene();
   private renderer?: WebGLRenderer;
   private smoke_particles: Mesh<PlaneGeometry, MeshLambertMaterial>[] = [];
-  private smoke_texture?: THREE.Texture;
   private frameId?: number;
   private texture_loader = new THREE.TextureLoader();
   private smoke_size = new THREE.PlaneGeometry(300, 300);
   private max_smoke_height = -200;
-  private min_smoke_height = (window.innerHeight / 2) * -1;
+  private min_smoke_height = (window.innerHeight / 2) * -1 * 2;
 
-
-  private readonly formBuilder = inject(FormBuilder);
-  public demoForm = this.formBuilder.group({
-    background_opacity: [1, Validators.required],
-    texture: ['smoke.png', Validators.required],
-    particle_count: [90, Validators.required],
-    particle_scale: [2, Validators.required],
-    opacity: [0.15, Validators.required],
-    emissive: ['#222222', Validators.required],
-    rotation_speed: [0.12, Validators.required],
-    rising_speed: [20, Validators.required],
-    fade_out_speed: [0.0005, Validators.required],
-    preset: ['', Validators.required]
-  });
+  public gui_options = {
+    background_opacity: 1,
+    texture_file: 'particle-smoke.png',
+    texture: new Texture(),
+    particle_count: 50,
+    particle_scale: 2,
+    opacity: 0.58,
+    emissive: '#5a7ce2',
+    rotation_speed: 0.37,
+    rising_speed: 20,
+    fade_out_speed: 0.001,
+    toggle_background: () => this.no_images = !this.no_images,
+    stop_effects: () => this.stop_effects()
+  }
 
   constructor() {
+    this.setup_gui_controls();
     this.camera.position.z = 1000;
     this.scene.fog = new THREE.FogExp2(0xc0f0ff, 0.0015);
+  }
 
-    this.demoForm.controls.particle_count.valueChanges.subscribe(value => {
-      const new_particle_count = +value!;
-      if (new_particle_count > this.smoke_particles.length) {
-        this.add_smoke_particles(new_particle_count - this.smoke_particles.length);
-      } else if (new_particle_count < this.smoke_particles.length) {
-        const difference = this.smoke_particles.length - new_particle_count;
-        this.smoke_particles.splice(-1 * difference).forEach((item, index) => {
-          this.scene.remove(item);
+  private async setup_gui_controls(): Promise<void> {
+    const gui = new GUI();
+    gui.add(this.gui_options, 'background_opacity', 0, 1, 0.1)
+    gui.add(this.gui_options, 'texture_file', ['particle-smoke.png', 'particle-fog.png'])
+      .onChange((v: string) => {
+        this.gui_options.texture = this.texture_loader.load(v);
+        this.smoke_particles.forEach(particle => {
+          particle.material = this.generate_material();
         })
-      }
-    })
-
-    this.demoForm.controls.preset.valueChanges.subscribe(value => {
-      if (value === undefined || value === null || value === '') {
-        this.demoForm.reset()
-        return;
-      }
-      this.demoForm.patchValue(presets[value]);
-    })
-
-    this.demoForm.controls.emissive.valueChanges.subscribe(value => {
-      this.demoForm.controls.emissive.setValue(value, {emitEvent: false});
-      this.smoke_particles.forEach(particle => {
-        particle.material = this.generate_material();
       })
-    })
-
-    this.demoForm.controls.particle_scale.valueChanges.subscribe(value => {
-      this.smoke_particles.forEach(particle => {
-        particle.scale.set(this.demoForm.value.particle_scale!, this.demoForm.value.particle_scale!, this.demoForm.value.particle_scale!);
-      })
-    })
-
-    merge(...[
-      this.demoForm.controls.texture.valueChanges,
-      this.demoForm.controls.opacity.valueChanges
-    ])
-      .pipe(
-        tap((v) => {
-          this.smoke_texture = this.texture_loader.load(this.demoForm.value.texture!);
-          this.smoke_particles.forEach(particle => {
-            particle.material = this.generate_material();
+    gui.add(this.gui_options, 'particle_count', 0, 600, 1)
+      .onChange((v: number) => {
+        const new_particle_count = +v!;
+        if (new_particle_count > this.smoke_particles.length) {
+          this.add_smoke_particles(new_particle_count - this.smoke_particles.length);
+        } else if (new_particle_count < this.smoke_particles.length) {
+          const difference = this.smoke_particles.length - new_particle_count;
+          this.smoke_particles.splice(-1 * difference).forEach((item, index) => {
+            this.scene.remove(item);
           })
+        }
+      })
+    gui.add(this.gui_options, 'particle_scale', 0.1, 2, 0.1)
+      .onChange((v: number) => {
+        this.smoke_particles.forEach(particle => {
+          particle.scale.set(this.gui_options.particle_scale, this.gui_options.particle_scale, this.gui_options.particle_scale);
         })
-      )
-      .subscribe()
+      })
+    gui.add(this.gui_options, 'opacity', 0, 1, 0.01)
+      .onChange((v: number) => {
+        this.smoke_particles.forEach(particle => {
+          particle.material = this.generate_material();
+        })
+      })
+    gui.addColor(this.gui_options, 'emissive')
+      .onChange(() => {
+        this.smoke_particles.forEach(particle => {
+          particle.material = this.generate_material();
+        })
+      });
+    gui.add(this.gui_options, 'rotation_speed', 0.01, 5, 0.02)
+    gui.add(this.gui_options, 'rising_speed', 1, 100, 1)
+    gui.add(this.gui_options, 'fade_out_speed', 0.001, 1, 0.001)
+
+    gui.add(this.gui_options, 'toggle_background'); // Button
+    gui.add(this.gui_options, 'stop_effects'); // Button
   }
 
   @HostBinding('class.no-images') no_images = false;
@@ -205,16 +213,34 @@ export class AppComponent implements OnInit, OnDestroy {
 
     this.renderer!.setPixelRatio(window.devicePixelRatio);
     this.renderer!.setSize(window.innerWidth, window.innerHeight);
-
+    this.drawHorizontalLine();
     const light = new THREE.HemisphereLight(0xd6e6ff, 0xa38c08, 1);
     this.scene.add(light);
 
-    this.smoke_texture = this.texture_loader.load(this.demoForm.value.texture!);
-    this.add_smoke_particles(+this.demoForm.value.particle_count!);
+    this.gui_options.texture = this.texture_loader.load(this.gui_options.texture_file);
+    this.add_smoke_particles(this.gui_options.particle_count!);
     this.renderer.setAnimationLoop(this.animate.bind(this));
   }
 
+  private async drawHorizontalLine(): Promise<void> {
+    const material = new LineBasicMaterial({color: 0xffff00}); // Yellow color
+
+    // const material = new LineMaterial({
+    //   color: 0xffff00, // Yellow color
+    //   linewidth: 3, // Line thickness
+    //   resolution: new Vector2(window.innerWidth, window.innerHeight), // Required for LineMaterial
+    // });
+    const points = [];
+    points.push(new Vector3(-1 * window.innerWidth, this.max_smoke_height, 0)); // Start point (adjust -10 for scene width)
+    points.push(new Vector3(window.innerWidth, this.max_smoke_height, 0)); // End point (adjust 10 for scene width)
+
+    const geometry = new BufferGeometry().setFromPoints(points);
+    const line = new Line(geometry, material);
+    this.scene.add(line);
+  }
+
   private add_smoke_particles(count: number) {
+    console.log('add_smoke_particles');
     new Array(count).fill('').forEach(() => {
       const smoke_material = this.generate_material();
       const smoke_particle = this.generate_particle(this.smoke_size, smoke_material);
@@ -230,16 +256,16 @@ export class AppComponent implements OnInit, OnDestroy {
    */
   private generate_material(opacity?: number): MeshLambertMaterial {
     return new THREE.MeshLambertMaterial({
-      map: this.smoke_texture,
-      emissive: new THREE.Color(this.demoForm.value.emissive!),
-      opacity: opacity ?? this.demoForm.value.opacity!,
+      map: this.gui_options.texture,
+      emissive: new THREE.Color(this.gui_options.emissive!),
+      opacity: opacity ?? this.gui_options.opacity!,
       transparent: true
     });
   }
 
   private generate_particle(size: PlaneGeometry, material: MeshLambertMaterial): Mesh<PlaneGeometry, MeshLambertMaterial> {
     const particle = new THREE.Mesh(size, material);
-    particle.scale.set(this.demoForm.value.particle_scale!, this.demoForm.value.particle_scale!, this.demoForm.value.particle_scale!);
+    particle.scale.set(this.gui_options.particle_scale!, this.gui_options.particle_scale, this.gui_options.particle_scale);
     const [x, y, z] = this.get_random_coordinates();
     particle.position.set(x, y, z);
     particle.rotation.z = Math.random() * 360;
@@ -248,7 +274,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private get_random_coordinates(): THREE.Vector3 {
     const x = random_number_in_range((window.innerWidth / 2) * -1, window.innerWidth / 2)
-    const y = random_number_in_range(this.min_smoke_height, this.max_smoke_height);
+    const y = random_number_in_range(this.min_smoke_height, window.innerHeight / 2 * -1);
     const z = Math.random() * 1000 - 100
     return new THREE.Vector3(x, y, z);
   }
@@ -258,27 +284,26 @@ export class AppComponent implements OnInit, OnDestroy {
     this.renderer!.render(this.scene, this.camera);
 
     this.smoke_particles.forEach((particle, index) => {
-      particle.rotation.z += (delta * this.demoForm.value.rotation_speed!);
-      particle.position.y += (delta * this.demoForm.value.rising_speed!);
-      if (particle.material.opacity < this.demoForm.value.opacity! && particle.position.y < this.max_smoke_height) {
-        particle.material.opacity += (delta * this.demoForm.value.fade_out_speed!);
+      // rotate and rise every particle
+      particle.rotation.z += (delta * this.gui_options.rotation_speed!);
+      particle.position.y += (delta * this.gui_options.rising_speed!);
+
+      // we need to fade out particles
+      // however, if the particle is above the threshold, lets fade it out 3x faster
+      if(particle.position.y >= this.max_smoke_height) {
+        particle.material.opacity -= this.gui_options.fade_out_speed * 3;
+      } else {
+        particle.material.opacity -= (this.gui_options.fade_out_speed * Math.random());
       }
 
-
-      /**
-       * if the particle is higher than the ceiling,
-       * lets start to fade out the smoke / mist
-       * when it is finally zero, we'll:
-       *  > reset the particle's
-       *  > put its opacity back to the original value
-       */
-      if (particle.position.y > this.max_smoke_height) {
-        const new_opacity = particle.material.opacity - (delta * this.demoForm.value.fade_out_speed!);
-        particle.material.opacity = new_opacity;
-        if (new_opacity < 0) {
-          const [x, y, z] = this.get_random_coordinates();
-          particle.position.set(x, y, z);
-        }
+      // once the particle is completely gone
+      // remove the particle and generate another one
+      // TODO: investigate if its worth while just updating its y-index and opacity
+      if (particle.material.opacity <= 0) {
+        this.scene.remove(particle);
+        this.smoke_particles.splice(index, 1);
+        console.log(this.smoke_particles.length)
+        this.add_smoke_particles(1)
       }
     })
   }
