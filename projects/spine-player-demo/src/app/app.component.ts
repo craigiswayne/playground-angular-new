28d4501b80ecd5eb7f6f71edd94d57798a08b529
Craig Wayne <craigiswayne@gmail.com>
2025-03-06 12:19:24 +0000

    Teefz animation added

--- projects/spine-player-demo/src/app/app.component.ts
+++ projects/spine-player-demo/src/app/app.component.ts
@@ -12,12 +12,12 @@ export class AppComponent implements AfterViewInit {
   public spine_test_ref = viewChild.required<ElementRef<HTMLDivElement>>('spine_test');
   private spinePlayer!: SpinePlayer;
   private guiControls = {
-    animation: 'walk',
+    animation: 'animation',
   }
   private lilGUI = new GUI();
 
   constructor() {
-    this.lilGUI.add(this.guiControls, 'animation', ['walk', 'jump', 'run', 'idle'])
+    this.lilGUI.add(this.guiControls, 'animation', ['animation', 'antiaipation', 'antiaipation2','Lose', 'win'])
       .onChange((v: string) => {
         this.spinePlayer.setAnimation(v);
         this.spinePlayer.play();
@@ -25,12 +25,15 @@ export class AppComponent implements AfterViewInit {
   }
   ngAfterViewInit() {
     this.spinePlayer = new SpinePlayer(this.spine_test_ref()!.nativeElement, {
-      skeleton: "/assets/spine/spineboy/spineboy-pro.json",
-      atlas: "/assets/spine/spineboy/spineboy-pma.atlas",
+      // skeleton: "/assets/spine/spineboy/spineboy-pro.json",
+      // atlas: "/assets/spine/spineboy/spineboy-pma.atlas",
+      skeleton: "/assets/spine/teefz/spine.skel",
+      atlas: "/assets/spine/teefz/spine.atlas",
       preserveDrawingBuffer: true,
       showControls: false,
       scale: 1,
-      success: () => {
+      success: (v) => {
+        console.log(v);
         this.spinePlayer.play();
       }
     });
